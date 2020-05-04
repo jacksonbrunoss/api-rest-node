91118d5ce4e1f3bdb783bc3c6404a7dd51e5e152
jacksonbrunoss <oi.jbss@gmail.com>
2020-05-04 10:34:41 -0300

    Protegendo as rotas com JWT

--- routes/produtos.js
+++ routes/produtos.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const mysql = require("../mysql").pool;
 const multer = require("multer");
+const login = require('../middleware/login');
 
 
 const storage = multer.diskStorage({
@@ -65,7 +66,7 @@ router.get("/", (req, res, next) => {
 });
 
 // INSERE UM PRODUTO
-router.post("/", upload.single(`produto_imagem`), (req, res, next) => {
+router.post("/", login.obrigatorio, upload.single(`produto_imagem`), (req, res, next) => {
   console.log(req.file)
   mysql.getConnection((error, conn) => {
     if (error) {
@@ -150,7 +151,7 @@ router.get("/:id_produto", (req, res, next) => {
 });
 
 // ALTERAR UM PRODUTO
-router.patch("/", (req, res, next) => {
+router.patch("/", login.obrigatorio, (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) {
       return res.status(500).send({
@@ -190,7 +191,7 @@ router.patch("/", (req, res, next) => {
 });
 
 // DELETAR UM PRODUTO
-router.delete("/", (req, res, next) => {
+router.delete("/", login.obrigatorio, (req, res, next) => {
   mysql.getConnection((error, conn) => {
     if (error) {
       return res.status(500).send({
