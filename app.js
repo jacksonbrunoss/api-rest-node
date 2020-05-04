003bf44126786f608b01c8bf1bec8adc6950bbf7
jacksonbrunoss <oi.jbss@gmail.com>
2020-05-03 23:09:00 -0300

    Cadastro de usuarios

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 const rotaProdutos = require("./routes/produtos");
 const rotaPedidos = require("./routes/pedidos");
+const rotaUsuarios = require("./routes/usuarios")
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({
@@ -27,6 +28,7 @@ app.use((req, res, next) => {
 
 app.use("/produtos", rotaProdutos);
 app.use("/pedidos", rotaPedidos);
+app.use("/usuarios", rotaUsuarios);
 
 app.use((req, res, next) => {
   const erro = new Error("Não encontrado");
